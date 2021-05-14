a673698e9630006a0dc50032c682ece1edff4b01
Thomas HOULLIER <a>
2021-05-14 12:39:14 +0200

    iter-some draft

--- src/iter.lisp
+++ src/iter.lisp
@@ -15,3 +15,21 @@
   (let ((res (iter (max (1- n-start) 1))))
     (loop for n from n-start upto n-end
           collect (setf res (* res n)))))
+
+;;; iter-some: Compute only some factorials.
+(defun iter-some (sorted-arglst)
+  "Compute the factorial iteratively for all the elements of sorted-arglst.
+   Input list must be sorted in increasing order."
+  (let ((res 1)
+        (last-el 0))
+    (loop for el in sorted-arglst do
+      (setf res (iter-step res (1+ last-el) el)
+            last-el el)
+          collect res)))
+
+(defun iter-step (res nnext nlast)
+  "Step the iteration from res = (nnext-1)! to nlast!."
+  (let ((reslast res))
+    (loop for i from nnext upto nlast do
+      (setf reslast (* reslast i)))
+    reslast))
